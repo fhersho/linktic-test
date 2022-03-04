e23bcd157ed37b927512d5254553c139d5ca5cec
fpadilla <fhersho@gmail.com>
2022-03-04 11:47:21 -0500

    add calculate stock feature

--- services/stock.service.js
+++ services/stock.service.js
@@ -5,18 +5,17 @@ const apikey = "c13a5d2ecf7cc6b8c50c06d7e1dfce22";
 const stocks = ["AAPL", "GOOGL", "AMZN", "TSLA", "FB", "TWTR", "UBER", "LYFT", "SNAP", "SHOP"];
 
 const calculateStocks = async () => {
-    try {
-        let total = 0;
-        for(stock of stocks) {
+    let total = 0;
+    for(stock of stocks) {
+        try {
             const res = (await (axios.get(`${urlApi}/${stock}?apikey=${apikey}`))).data;
             total = total + res[0].price;
+        } catch (e) {
+            console.log(e.message);
         }
-        console.log(total);
-        return total;
-    } catch (e) {
-        console.log(e.message);
     }
-    return 0;
+
+    return total;
 }
 
 module.exports = {calculateStocks}
